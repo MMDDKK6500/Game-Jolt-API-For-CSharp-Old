499a27aa1fe7226c9d171a52ac8b4bd635b7d185
MMDDKK <34542949+MMDDKK6500@users.noreply.github.com>
2021-01-19 12:24:24 -0300

    Update Program.cs

--- apitest/Program.cs
+++ apitest/Program.cs
@@ -7,8 +7,6 @@ namespace apitest
     {
         static void Main(string[] args)
         {
-            Console.Write(Time.getTime("499308", "4adc3e1c1df4fe3581cbe114b58633fe"));
-            Console.WriteLine(User.fetchUser("499308", "4adc3e1c1df4fe3581cbe114b58633fe", "MMDDKK"));
             Console.ReadKey();
         }
     }
