f44a8426023c06ff2d4dbb26c35b69022fc4c251
WoobieGames <woobiegames@gmail.com>
2021-01-20 12:24:08 -0300

    Updated apitest for tests

--- apitest/Program.cs
+++ apitest/Program.cs
@@ -7,6 +7,13 @@ namespace apitest
     {
         static void Main(string[] args)
         {
+            string game_id = "499308";
+            string private_key = "my_private_key";
+            string username = "MMDDKK";
+            string token = "don't share your token pls";
+            //Use this project to test the lib
+            //https://gamejolt.com/game-api/doc here you can get every command(Each category is a class)
+            Console.WriteLine(User.authUser(game_id, private_key, username, token));
             Console.ReadKey();
         }
     }
